6857977c7d97507ef5ffe2ab72495cac399d5b46
Aleksandr Zaruchevskii <zaruczewski@gmail.com>
2026-06-27 12:13:45 +0300

    ci(commitlint): exempt dependency bump headers from length rule

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,4 +1,7 @@
 export default {
   extends: ["@commitlint/config-conventional"],
-  ignores: [(msg) => /Signed-off-by: dependabot\[bot]/m.test(msg)],
+  ignores: [
+    (msg) => /Signed-off-by: dependabot\[bot]/m.test(msg),
+    (msg) => /^\w+\(deps\): bump .+ from .+ to .+/.test(msg),
+  ],
 };
